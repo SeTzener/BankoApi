b92130da750db866dcc0e27ad18b76edef9a3641
Gaetano Zito <gv.zito@hotmail.com>
2025-01-04 02:33:36 +0100

    First commit

--- BankoApi/obj/Debug/net9.0/BankoApi.AssemblyInfo.cs
+++ BankoApi/obj/Debug/net9.0/BankoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b13ddd6f5bc235800faa97db6604564f5860f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
