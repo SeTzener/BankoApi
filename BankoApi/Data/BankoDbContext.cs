27c481f9d0f20c076ecc2de04683df056ec35a18
setzener <gaetanovf@gmail.com>
2025-01-07 04:52:58 +0100

    DB context

---
+++ BankoApi/Data/BankoDbContext.cs
@@ -0,0 +1,28 @@
+using BankoApi.Data.Dao;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankoApi.Data;
+
+public class BankoDbContext : DbContext
+{
+    public DbSet<Institution> Institutions { get; set; }
+    public DbSet<Balance> Balances { get; set; }
+    public DbSet<Requisition> Requisitions { get; set; }
+    public DbSet<Transactions> Transactions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        Env.Load();
+
+        var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "";
+        var dbPassword = Environment.GetEnvironmentVariable("DB_PASS") ?? "";
+
+        var connectionString =
+            $"Server=localhost,1433;Database=BankoDb;User Id={dbUser};Password={dbPassword};TrustServerCertificate=True;";
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
