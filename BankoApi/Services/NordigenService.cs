2823c6f377f2b9f2c04e4a6dd2782cbccdc8a61f
setzener <gaetanovf@gmail.com>
2025-01-09 03:15:38 +0100

    Nordigen service

---
+++ BankoApi/Services/NordigenService.cs
@@ -0,0 +1,42 @@
+using System.Net.Http.Headers;
+using BankoApi.Data.Dao;
+
+namespace BankoApi.Services;
+
+public class NordigenService
+{
+    private readonly HttpClient _httpClient;
+    private readonly NordigenTokenService _tokenService;
+    private readonly ILogger<NordigenService> _logger;
+    
+    public NordigenService(HttpClient httpClient, NordigenTokenService tokenService, ILogger<NordigenService> logger)
+    {
+        _httpClient = httpClient; 
+        _tokenService = tokenService;
+        _logger = logger;
+    }
+
+    // TODO():Change this Transactions from DAO to a Model dto
+    public async Task<List<Transactions>> GetTransactionsAsync(string accountId)
+    {
+        // TODO(): Manage failures
+        var token = await _tokenService.GetAccessTokenAsync();
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _httpClient.GetAsync($"accounts/{accountId}/transactions");
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<NordigenTransactionResponse>();
+        return result?.Transactions.Select(t => new Transactions
+        {
+            BankTransactions = t.BankTransactions
+        }).ToList() ?? new List<Transactions>();
+    }
+}
+
+// TODO(): This response has to be written better
+public class NordigenTransactionResponse
+{
+    public List<Transactions> Transactions { get; set; }
+}
