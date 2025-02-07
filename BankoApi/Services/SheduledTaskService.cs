bd3ea7f69d9c27185f156b794d99f58789463a80
setzener <gaetanovf@gmail.com>
2025-02-07 23:52:47 +0100

    Scheduler fix

--- BankoApi/Services/SheduledTaskService.cs
+++ BankoApi/Services/SheduledTaskService.cs
@@ -1,11 +1,15 @@
+using System.Text.Json;
 using BankoApi.Controllers.GoCardless;
+using BankoApi.Data;
+using BankoApi.Repository;
+using BankoApi.Services.Model;
 using DotNetEnv;
 
 namespace BankoApi.Services;
 
 public class ScheduledTaskService : BackgroundService
 {
-    private readonly TimeSpan _executionTime = new(08, 00, 00);
+    private readonly TimeSpan _executionTime = new(22, 48, 00);
     private readonly IServiceScopeFactory _scopeFactory;
 
     public ScheduledTaskService(IServiceScopeFactory scopeFactory)
@@ -15,10 +19,6 @@ public class ScheduledTaskService : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        Env.Load();
-        // TODO(): Change this to retrieve a list of accountIDs from the database
-        var accountId = Environment.GetEnvironmentVariable("GOCARDLESS_ACCOUNT_ID") ??
-                        throw new Exception("Environment variable GOCARDLESS_ACCOUNT_ID not set");
         while (!stoppingToken.IsCancellationRequested)
         {
             var now = DateTime.UtcNow;
@@ -33,17 +33,36 @@ public class ScheduledTaskService : BackgroundService
 
             try
             {
-                using var scope = _scopeFactory.CreateScope();
-                var transactionsController = scope.ServiceProvider.GetRequiredService<TransactionsController>();
-
-                // Trigger the endpoint
-                await transactionsController.FetchAndStoreTransactions(accountId);
+                FetchAndStoreTransactions();
             }
             catch (Exception ex)
             {
                 // Handle exceptions (log errors, etc.)
                 Console.WriteLine($"Error occurred: {ex.Message}");
             }
+        }
+    }
+
+    private async void FetchAndStoreTransactions()
+    {
+        Env.Load();
+        // TODO(): Change this to retrieve a list of accountIDs from the database
+        Console.WriteLine(DateTime.UtcNow);
+        var accountId = Environment.GetEnvironmentVariable("GOCARDLESS_ACCOUNT_ID") ??
+                        throw new Exception("Environment variable GOCARDLESS_ACCOUNT_ID not set");
+        
+        using var scope = _scopeFactory.CreateScope();
+        var goCardlessService = scope.ServiceProvider.GetRequiredService<GoCardlessService>();
+        var dbContext = scope.ServiceProvider.GetRequiredService<BankoDbContext>();
+
+
+        // Trigger the endpoint
+        var transactions = await goCardlessService.GetTransactionsAsync(accountId);
+        if (transactions != null)
+        {
+            var repository = new TransactionsRepository();
+            repository.StoreTransactions(dbContext, transactions);
+            await dbContext.SaveChangesAsync();
         }
     }
 }
