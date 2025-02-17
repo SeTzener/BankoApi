c53896057a2389dd0de5ab5b9ab77b2b0fcbed18
setzener <gaetanovf@gmail.com>
2025-02-17 21:15:42 +0100

    Enhances nullability of the ExpenseTagId

--- BankoApi/Controllers/BankoApi/Requests/UpdateExpanseTagRequest.cs
+++ BankoApi/Controllers/BankoApi/Requests/UpdateExpanseTagRequest.cs
@@ -2,6 +2,6 @@ namespace BankoApi.Controllers.BankoApi.Requests;
 
 public class UpdateExpenseTagRequest
 {
-    public string ExpenseTagId { get; set; }
+    public string? ExpenseTagId { get; set; }
     public string TransactionId { get; set; }
 }
