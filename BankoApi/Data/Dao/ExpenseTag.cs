00fabd1e362f1b099309a9988476f744c4c59631
SeTzener <gaetanovf@gmail.com>
2025-05-28 14:59:12 +0200

    Add isEarning flag (#17)

--- BankoApi/Data/Dao/ExpenseTag.cs
+++ BankoApi/Data/Dao/ExpenseTag.cs
@@ -5,5 +5,6 @@ public class ExpenseTag
     public String Id { get; set; }
     public required String Name { get; set; }
     public required Int64 Color { get; set; }
+    public required Boolean isEarning { get; set; } = false;
     public List<String>? Aka { get; set; }
 }
