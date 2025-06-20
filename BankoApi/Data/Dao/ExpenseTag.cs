d939ba8b45f24bee6ccd59278308720843336113
setzener <gaetanovf@gmail.com>
2025-06-20 14:16:15 +0200

    Transaction are stored by bookingdate

--- BankoApi/Data/Dao/ExpenseTag.cs
+++ BankoApi/Data/Dao/ExpenseTag.cs
@@ -2,9 +2,9 @@ namespace BankoApi.Data.Dao;
 
 public class ExpenseTag
 {
-    public String Id { get; set; }
-    public required String Name { get; set; }
-    public required Int64 Color { get; set; }
-    public required Boolean isEarning { get; set; } = false;
-    public List<String>? Aka { get; set; }
+    public string Id { get; set; }
+    public required string Name { get; set; }
+    public required long Color { get; set; }
+    public required bool isEarning { get; set; } = false;
+    public List<string>? Aka { get; set; }
 }
